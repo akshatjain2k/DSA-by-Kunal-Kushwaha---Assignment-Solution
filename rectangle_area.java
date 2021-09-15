52ee9e9d7a8067216bf0fee2775e357ec2fc11a0
Akshat Jain <67467019+akshatjain2k@users.noreply.github.com>
2021-09-14 22:21:45 -0700

    Add files via upload

---
+++ rectangle_area.java
@@ -0,0 +1,21 @@
+// 3. Area Of Rectangle Program 
+
+
+import java.util.Scanner;
+
+public class rectangle_area {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        float w,l;
+        double area;
+        System.out.print("Length of Rectangle: ");
+        l = in.nextFloat();
+        System.out.print("Width of Rectangle: ");
+        w = in.nextFloat();
+        
+        area = l*w;
+        System.out.println("Area of Rectangle: "+area);
+    }
+}
