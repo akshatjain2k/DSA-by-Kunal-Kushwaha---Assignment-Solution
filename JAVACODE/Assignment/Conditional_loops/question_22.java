90d35d98e8aa890ac0a61013a3d2e51ab7d62927
akshatjain2k <akshatjain295@gmail.com>
2021-09-17 21:45:47 +0530

    6th commit

--- JAVACODE/Assignment/Conditional_loops/question_22.java
+++ JAVACODE/Assignment/Conditional_loops/question_22.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 //22. [Subtract the Product and Sum of Digits of an Integer](https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/)
 public class question_22 {
-    int n = 237;
+    public static void main(String[] args) {
+        int num,digit,prod=1,sum=0,result=0;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Any Number: ");
+        num = in.nextInt();
+
+        while(num>0){
+            digit = num % 10;
+            sum = sum + digit;
+            prod = prod*digit;
+            result = prod - sum;
+            num = num/10;
+        }
+        System.out.println("Sum: "+sum);
+        System.out.println("Product: "+prod);
+        System.out.println("Product - Sum: "+result);
+    }
     
 }
