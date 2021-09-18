6544906ded54fad7a3e8ad5638ecd6cdb5d16cb3
akshatjain2k <akshatjain295@gmail.com>
2021-09-18 12:42:45 +0530

    8th Commit

--- JAVACODE/Assignment/Conditional_loops/question_25_Take_integer_inputs_till_the_user_enters_0_and_print_the_largest_number_from_all.java
+++ JAVACODE/Assignment/Conditional_loops/question_25_Take_integer_inputs_till_the_user_enters_0_and_print_the_largest_number_from_all.java
@@ -1,11 +1,13 @@
-//24. Take_integer_inputs_till_the_user_enters_0_and_print_the_sum_of_all_numbers
+
+//25. Take integer inputs till the user enters 0 and print the largest number from all.
 
 import java.util.Scanner;
 
-public class question_24_Take_integer_inputs_till_the_user_enters_0_and_print_the_sum_of_all_numbers {
+public class question_25_Take_integer_inputs_till_the_user_enters_0_and_print_the_largest_number_from_all {
+
     public static void main(String[] args) {
         
-        int sum=0;
+        int max=0,sum=0;
         Scanner in = new Scanner(System.in);
         // System.out.print("Enter Any Number: ");
         // n = in.nextInt();
@@ -16,12 +18,14 @@ public class question_24_Take_integer_inputs_till_the_user_enters_0_and_print_th
 
             if(n>0)
                 sum = sum + n;
-            
+                if (n>max) {
+                    max=n;
+                }
             else
                 break;
-            
      }
      System.out.println("Sum of All the element: "+sum);
+     System.out.println("Largest Number: "+max);
 
  }}
 
